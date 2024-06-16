5bdcb7208ed1624d614d1d9c4af8819c3adc373a
Alpha-Oumar BARRY <alpha-oumar.barry@outlook.com>
2024-06-16 14:13:32 +0200

    refacto code

--- src/app/components/App.tsx
+++ src/app/components/App.tsx
@@ -1,5 +1,5 @@
 import MovieList from '../components/MovieList';
-import FilterSelect from '../components/FilterSelect';
+import FilterSelect from './FilterSelect/FilterSelect';
 import { fetchMovies } from '../redux/slices/moviesSlice';
 import { useDispatch } from 'react-redux';
 import { useEffect } from 'react';
